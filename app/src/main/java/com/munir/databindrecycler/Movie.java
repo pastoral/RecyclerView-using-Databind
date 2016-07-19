7920b35e306f2193924be30a6e9d4dc898e64eff
unknown <munirul.hoque@EG-0101040258.edison-bd.com>
2016-07-19 15:44:23 +0600

    POJO and sample data added with observable field

---
+++ app/src/main/java/com/munir/databindrecycler/Movie.java
@@ -0,0 +1,16 @@
+package com.munir.databindrecycler;
+
+/**
+ * Created by munirul.hoque on 7/19/2016.
+ */
+public class Movie {
+    private String title, genre, year;
+    public Movie(){
+    }
+
+    public Movie(String title, String genre, String year) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+    }
+}
---
+++ app/src/main/java/com/munir/databindrecycler/MovieInfoData.java
@@ -0,0 +1,72 @@
+package com.munir.databindrecycler;
+
+import android.databinding.ObservableArrayList;
+import android.view.View;
+
+/**
+ * Created by munirul.hoque on 7/19/2016.
+ */
+public class MovieInfoData {
+    public ObservableArrayList<Movie> movieList = new ObservableArrayList<>();
+
+    public MovieInfoData(){
+        Movie movie = new Movie("Mad Max: Fury Road", "Action & Adventure", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("Inside Out", "Animation, Kids & Family", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("Shaun the Sheep", "Animation", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("The Martian", "Science Fiction & Fantasy", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("Mission: Impossible Rogue Nation", "Action", "2015");
+        movieList.add(movie);
+
+        movie = new Movie("Up", "Animation", "2009");
+        movieList.add(movie);
+
+        movie = new Movie("Star Trek", "Science Fiction", "2009");
+        movieList.add(movie);
+
+        movie = new Movie("The LEGO Movie", "Animation", "2014");
+        movieList.add(movie);
+
+        movie = new Movie("Iron Man", "Action & Adventure", "2008");
+        movieList.add(movie);
+
+        movie = new Movie("Aliens", "Science Fiction", "1986");
+        movieList.add(movie);
+
+        movie = new Movie("Chicken Run", "Animation", "2000");
+        movieList.add(movie);
+
+        movie = new Movie("Back to the Future", "Science Fiction", "1985");
+        movieList.add(movie);
+
+        movie = new Movie("Raiders of the Lost Ark", "Action & Adventure", "1981");
+        movieList.add(movie);
+
+        movie = new Movie("Goldfinger", "Action & Adventure", "1965");
+        movieList.add(movie);
+
+        movie = new Movie("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
+        movieList.add(movie);
+    }
+
+
+    public void add(Movie movie){
+        movieList.add(movie);
+    }
+
+    public void remove(View view){
+        if(!movieList.isEmpty()){
+            movieList.remove(0);
+        }
+    }
+}

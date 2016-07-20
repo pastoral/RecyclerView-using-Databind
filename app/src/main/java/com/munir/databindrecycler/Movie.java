7e8a0495e48ed20f7d8b438b43d8767bc22cd7fd
unknown <munirul.hoque@EG-0101040258.edison-bd.com>
2016-07-20 16:00:25 +0600

    POJO and smaple data list modified

--- app/src/main/java/com/munir/databindrecycler/Movie.java
+++ app/src/main/java/com/munir/databindrecycler/Movie.java
@@ -4,11 +4,13 @@ package com.munir.databindrecycler;
  * Created by munirul.hoque on 7/19/2016.
  */
 public class Movie {
+    public int posterMovie; /* Resource id for the image */
     private String title, genre, year;
     public Movie(){
     }
 
-    public Movie(String title, String genre, String year) {
+    public Movie(int posterMovie,String title, String genre, String year) {
+        this.posterMovie = posterMovie;
         this.title = title;
         this.genre = genre;
         this.year = year;
--- app/src/main/java/com/munir/databindrecycler/MovieInfoData.java
+++ app/src/main/java/com/munir/databindrecycler/MovieInfoData.java
@@ -10,52 +10,52 @@ public class MovieInfoData {
     public ObservableArrayList<Movie> movieList = new ObservableArrayList<>();
 
     public MovieInfoData(){
-        Movie movie = new Movie("Mad Max: Fury Road", "Action & Adventure", "2015");
+        Movie movie = new Movie(android.R.drawable.sym_def_app_icon, "Mad Max: Fury Road", "Action & Adventure", "2015");
         movieList.add(movie);
 
-        movie = new Movie("Inside Out", "Animation, Kids & Family", "2015");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Inside Out", "Animation, Kids & Family", "2015");
         movieList.add(movie);
 
-        movie = new Movie("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Star Wars: Episode VII - The Force Awakens", "Action", "2015");
         movieList.add(movie);
 
-        movie = new Movie("Shaun the Sheep", "Animation", "2015");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Shaun the Sheep", "Animation", "2015");
         movieList.add(movie);
 
-        movie = new Movie("The Martian", "Science Fiction & Fantasy", "2015");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "The Martian", "Science Fiction & Fantasy", "2015");
         movieList.add(movie);
 
-        movie = new Movie("Mission: Impossible Rogue Nation", "Action", "2015");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Mission: Impossible Rogue Nation", "Action", "2015");
         movieList.add(movie);
 
-        movie = new Movie("Up", "Animation", "2009");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Up", "Animation", "2009");
         movieList.add(movie);
 
-        movie = new Movie("Star Trek", "Science Fiction", "2009");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Star Trek", "Science Fiction", "2009");
         movieList.add(movie);
 
-        movie = new Movie("The LEGO Movie", "Animation", "2014");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "The LEGO Movie", "Animation", "2014");
         movieList.add(movie);
 
-        movie = new Movie("Iron Man", "Action & Adventure", "2008");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Iron Man", "Action & Adventure", "2008");
         movieList.add(movie);
 
-        movie = new Movie("Aliens", "Science Fiction", "1986");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Aliens", "Science Fiction", "1986");
         movieList.add(movie);
 
-        movie = new Movie("Chicken Run", "Animation", "2000");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Chicken Run", "Animation", "2000");
         movieList.add(movie);
 
-        movie = new Movie("Back to the Future", "Science Fiction", "1985");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Back to the Future", "Science Fiction", "1985");
         movieList.add(movie);
 
-        movie = new Movie("Raiders of the Lost Ark", "Action & Adventure", "1981");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Raiders of the Lost Ark", "Action & Adventure", "1981");
         movieList.add(movie);
 
-        movie = new Movie("Goldfinger", "Action & Adventure", "1965");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Goldfinger", "Action & Adventure", "1965");
         movieList.add(movie);
 
-        movie = new Movie("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
+        movie = new Movie(android.R.drawable.sym_def_app_icon, "Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
         movieList.add(movie);
     }
 
